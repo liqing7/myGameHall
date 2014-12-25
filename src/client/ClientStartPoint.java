4f0750a9d5b6475c42f61f799821025685abcac2
liqing7 <remmus@foxmail.com>
2014-12-25 21:20:01 +0800

    client start point

---
+++ src/client/ClientStartPoint.java
@@ -0,0 +1,10 @@
+package client;
+
+import userInterface.LoginFrame;
+
+public class ClientStartPoint {
+	
+	public static void main(String[] args) {
+		LoginFrame loginFrame = new LoginFrame();
+	}
+}
